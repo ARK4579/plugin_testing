e0e7a56343c0bd2fb05f6aeef9783fe87e17880d
ark <ark4579@hotmail.com>
2023-11-08 14:24:19 +0500

    updates

---
+++ wordpress/settings.py
@@ -0,0 +1,22 @@
+import json
+
+_settings = {
+    "DATABSE_NAME": "",
+    "DB_USER_NAME": "",
+    "DB_USER_PASS": "",
+    "ADMIN_NAME": "",
+    "ADMIN_PASS": "",
+    "HTDOCS_PATH": "",
+}
+try:
+    with open("settings.json") as f:
+        _settings = json.load(f)
+except FileNotFoundError:
+    print("No _settings found. Falling back to default settings!")
+
+DATABSE_NAME = _settings.get("DATABSE_NAME", "")
+DB_USER_NAME = _settings.get("DB_USER_NAME", "")
+DB_USER_PASS = _settings.get("DB_USER_PASS", "")
+ADMIN_NAME = _settings.get("ADMIN_NAME", "")
+ADMIN_PASS = _settings.get("ADMIN_PASS", "")
+HTDOCS_PATH = _settings.get("HTDOCS_PATH", "")
---
+++ wordpress/testing.py
@@ -0,0 +1,28 @@
+def clear_database():
+    # reset all wordpress dabase and tabeles
+    pass
+
+
+def uninstall_wp():
+    # un install all wp installations
+    pass
+
+
+def create_database():
+    # create new wp database
+    pass
+
+
+def install_wp():
+    pass
+
+
+def main():
+    clear_database()
+    uninstall_wp()
+    create_database()
+    install_wp()
+
+
+if __name__ == "__main__":
+    main()
